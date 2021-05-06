dffc937cb45b256bcbe1805df30c4c8ba158aaf4
izak <izabkru88@gmail.com>
2021-05-06 16:45:57 +0100

    Added  sticky Navbar that hides on scroll.

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,10 +1,33 @@
 import Link from 'next/link';
 import classes from './main-navigation.module.css';
 import Logo from './logo';
+import { debounce } from '../utilities/helpers';
+import React, { useState, useEffect } from 'react';
 
 export default function MainNavigation() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
-    <header className={classes.header}>
+    <header className={classes.header} style={{ top: visible ? '0' : '-8rem' }}>
       <Link href='/'>
         <a>
           <Logo />
