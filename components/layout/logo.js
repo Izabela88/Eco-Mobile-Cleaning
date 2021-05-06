a72f440302f9fe371b7d73eef5b1b3b1b5f97eb6
izak <izabkru88@gmail.com>
2021-05-06 12:18:42 +0100

    Styling Logo.

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -3,8 +3,8 @@ import classes from './logo.module.css';
 
 export default function Logo() {
   return (
-    <div>
-      <Image src='/images/Logo1.png' alt='A Logo' width={50} height={50} />
+    <div className={classes.logo}>
+      <Image src='/images/Logo1.png' alt='A Logo' width={100} height={100} />
     </div>
   );
 }
