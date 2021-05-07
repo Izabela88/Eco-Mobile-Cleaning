12d8fd8c09373c14fe150601fd6efd43766a25dc
izak <izabkru88@gmail.com>
2021-05-07 14:06:41 +0100

    Added image.

--- components/about/about.js
+++ components/about/about.js
@@ -4,8 +4,13 @@ import classes from './about.module.css';
 export default function About() {
   return (
     <section className={classes.about}>
-      <div>
-        <Image />
+      <div className={classes.image1}>
+        <Image
+          src='/images/img6.jpg'
+          alt='About image'
+          width={500}
+          height={500}
+        />
       </div>
     </section>
   );
