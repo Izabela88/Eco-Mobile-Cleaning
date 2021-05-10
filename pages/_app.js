9f57d2e3f490210bd0e8eb0087dd91eb7d8a636a
izak <izabkru88@gmail.com>
2021-05-10 11:48:07 +0100

    Added Sanity to project.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import Layout from '../components/layout/layout';
-import { RouterScrollProvider } from '@moxy/next-router-scroll';
 
 function MyApp({ Component, pageProps }) {
   return (
