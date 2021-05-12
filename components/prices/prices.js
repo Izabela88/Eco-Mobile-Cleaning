c332fd04890f26b578af0eff7ef39045e4154b97
izak <izabkru88@gmail.com>
2021-05-12 16:38:03 +0100

    Styling cards and Prices section (cards border)

--- components/prices/prices.js
+++ components/prices/prices.js
@@ -4,6 +4,12 @@ import Cards from './cards';
 export default function Prices() {
   return (
     <div className={classes.prices}>
+      <div className={classes.heading}>
+        <h1>
+          <span>Our</span> Prices
+        </h1>
+        <h2>and services</h2>
+      </div>
       <Cards />
     </div>
   );
